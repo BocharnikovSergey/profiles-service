e13f25d9bedb7a0ad8e61279e36014dc603a4d1d
Viktor Balonkin <v.balonkin@gmail.com>
2026-08-11 20:28:46 +0300

    https://ru.yougile.com/team/d193f0b54f05/#BAC-100

--- tests/test_profiles_routes.py
+++ tests/test_profiles_routes.py
@@ -16,7 +16,6 @@ def expected_payload(**overrides):
         "city": None,
         "citizenship": None,
         "currency": None,
-        "avatar_url": None,
     }
     payload.update(overrides)
     return payload
@@ -39,7 +38,7 @@ class StubProfileManager:
             "city": None,
             "citizenship": None,
             "currency": None,
-            "avatar_url": None,
+
             "created_at": datetime(2024, 1, 1, tzinfo=timezone.utc),
             "updated_at": datetime(2024, 1, 1, tzinfo=timezone.utc),
         }
