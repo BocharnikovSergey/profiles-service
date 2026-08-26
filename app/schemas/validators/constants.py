c497b5799b77a15fe5446c2b920dafdcae33c1d3
Sergey Bocharnikov <bocharnikov_sergey@mail.ru>
2026-08-26 16:28:31 +0300

    https://ru.yougile.com/team/d193f0b54f05/#BAC-118 - add_validate_phone_numder

---
+++ app/schemas/validators/constants.py
@@ -0,0 +1,1 @@
+ALLOWED_REGIONS = {"RU", "KZ"}
---
+++ app/schemas/validators/phone_number.py
@@ -0,0 +1,31 @@
+import phonenumbers
+from phonenumbers import NumberParseException
+
+from .constants import ALLOWED_REGIONS
+
+
+def normalize_phone_number(phone_number: str) -> str:
+    """
+    Проверяет номер телефона и приводит его к формату E.164.
+
+    Номер должен быть валидным и относиться к одной из поддерживаемых стран.
+
+    :param phone_number: Номер телефона для проверки и нормализации.
+    :return: Номер телефона в формате E.164.
+    :raises ValueError: Если номер некорректен или страна не поддерживается.
+    """
+    try:
+        phone_parse = phonenumbers.parse(phone_number, None)
+    except NumberParseException:
+        raise ValueError("Invalid phone number")
+    if not phonenumbers.is_valid_number(phone_parse):
+        raise ValueError("Invalid phone number")
+    elif not ALLOWED_REGIONS.intersection(
+        phonenumbers.region_codes_for_country_code(
+            phone_parse.country_code
+        )
+    ):
+        raise ValueError("Phone number country is not supported")
+    return phonenumbers.format_number(
+        phone_parse, phonenumbers.PhoneNumberFormat.E164
+        )
